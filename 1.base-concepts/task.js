c857db94ebf244c55d4f1d4b627c3ed37fef4804
Yuliya Kalinina <u.v.zaika@gmail.com>
2024-05-03 12:21:22 +0400

    Первое задание выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr = [-b / (2 * a)];
+  } else if (d > 0) {
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let P = percent / 100 / 12;
+  let S = amount - contribution;
+  let monthPayment = S * (P + P / ((1 + P) ** countMonths - 1));
+
+  let totalAmount = (monthPayment * countMonths).toFixed(2);
+
+  return Number(totalAmount);
 }
