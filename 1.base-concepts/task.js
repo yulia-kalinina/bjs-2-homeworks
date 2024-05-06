2b05b39ebc1abf60381c43624fcb79e8d035a6b7
Yuliya Kalinina <u.v.zaika@gmail.com>
2024-05-06 09:36:27 +0400

    Первое задание исправлено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,9 +3,7 @@ function solveEquation(a, b, c) {
   let arr = [];
   let d = b ** 2 - 4 * a * c;
 
-  if (d < 0) {
-    arr = [];
-  } else if (d === 0) {
+  if (d === 0) {
     arr = [-b / (2 * a)];
   } else if (d > 0) {
     arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
@@ -15,11 +13,12 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let P = percent / 100 / 12;
-  let S = amount - contribution;
-  let monthPayment = S * (P + P / ((1 + P) ** countMonths - 1));
+  let percentConverted = percent / 100 / 12;
+  let refundedAmount = amount - contribution;
+  let monthPayment =
+    refundedAmount *
+    (percentConverted +
+      percentConverted / ((1 + percentConverted) ** countMonths - 1));
 
-  let totalAmount = (monthPayment * countMonths).toFixed(2);
-
-  return Number(totalAmount);
+  return Number((monthPayment * countMonths).toFixed(2));
 }
