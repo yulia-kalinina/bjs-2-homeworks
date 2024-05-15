31e757178035d016d1b7b22b86328f34c28451ed
Yuliya Kalinina <u.v.zaika@gmail.com>
2024-05-15 12:25:31 +0400

    Четвертое задание готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
+
+let student1 = new Student("Василиса", "женский", 19);
+let student2 = new Student("Артём", "мужской", 25);
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+  this.subject = subjectName;
+};
 
 Student.prototype.addMarks = function (...marks) {
-  
-}
+  if (this.marks !== undefined) {
+    this.marks.push(...marks);
+  }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+  if (this.marks === undefined) {
+    return 0;
+  } else {
+    return this.marks.reduce(
+      (acc, mark, index, marks) => acc + mark / marks.length,
+      0
+    );
+  }
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+  delete this.subjectName;
+  delete this.marks;
+  this.excluded = reason;
+};
